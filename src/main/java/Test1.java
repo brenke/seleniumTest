1f36cec132a143f4f4b1434bf4b9951ead56eaa0
Andrey <79911219+brenke@users.noreply.github.com>
2023-02-10 12:31:33 +0300

    code update

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -15,14 +15,10 @@ public class Test1 {
         System.setProperty("webdriver.chrome.driver", "D:\\chromedriver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://google.com");
+        driver.get("https://testpages.herokuapp.com/");
     }
     @Test
-    public void testExample() {
-        WebElement input = driver.findElement(By.xpath("//input[@title='Поиск']"));
-        input.click();
-        input.sendKeys("testpages herokuapp");
-        input.sendKeys(Keys.ENTER);
+    public void testAlert() {
         driver.findElement(By.xpath("//h3[@class='LC20lb MBeuO DKV0Md']")).click();
         driver.findElement(By.id("alerttest")).click();
         driver.findElement(By.xpath("//input[@value='Show confirm box']")).click();
@@ -30,4 +26,8 @@ public class Test1 {
         WebElement result = driver.findElement(By.xpath("//p[@id='confirmreturn']"));
         Assertions.assertEquals("true",result.getText());
     }
-}
+	
+	//@Test
+	//public void testTable() {
+		//driver.findElement(By.xpath("//a[@id='tablestest']")).click();
+	//}
